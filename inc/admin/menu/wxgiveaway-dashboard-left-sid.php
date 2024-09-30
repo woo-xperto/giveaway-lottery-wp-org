fa171f9c8413ace09b286acc16fdf08f2bd3b772
Woo-Xperto <153199501+Woo-Xperto@users.noreply.github.com>
2024-09-30 15:20:06 +0600

    HEREDOC or NOWDOC fix

--- inc/admin/menu/wxgiveaway-dashboard-left-sid.php
+++ inc/admin/menu/wxgiveaway-dashboard-left-sid.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'wxgiveaway_setting_page.php';
-// === >>>> Dashboard Left side menu <<<< === \\
+// Dashboard Left side menu 
 if(!function_exists('wxgiveaway_wp_admin_dashboard_menu_reg_pro')){
   add_action("admin_menu", "wxgiveaway_wp_admin_dashboard_menu_reg");
   function wxgiveaway_wp_admin_dashboard_menu_reg() {
@@ -21,53 +21,6 @@ if(!function_exists('wxgiveaway_wp_admin_dashboard_menu_reg_pro')){
         'setting', // sub menu slug
         'wxgiveaway_setting_page' // sub meun funciton for page
       );
-      // Add the first submenu item with a different title
-      /*add_submenu_page(
-        'giveaway-system', // parent slug
-        __('Overview','giveaway-lottery'), // page title
-        __('Overview','giveaway-lottery'), // submenu title
-        'manage_options', // capability
-        'giveaway-system', // slug for submenu
-        'wxgiveaway_overview_page' // function for submenu page
-      );
-
-      //add submenu 2
-      add_submenu_page(
-        'giveaway-system', // parent menu slug
-        __('Giveaways','giveaway-lottery'), // Page title
-        'Giveaways', // Menu title
-        'manage_options',  // Capability
-        'giveaways', // sub menu slug
-        'wxgiveaway_giveaway_page' // sub meun funciton for page
-      );
-      //add submenu 3
-      add_submenu_page(
-        'giveaway-system', // parent menu slug
-        __('Winners','giveaway-lottery'), // Page title
-        'Winners', // Menu title
-        'manage_options',  // Capability
-        'winners', // sub menu slug
-        'wxgiveaway_winners_page' // sub meun funciton for page
-      );
-      //add submenu 4
-      add_submenu_page(
-        'giveaway-system', // parent menu slug
-        __('Report','giveaway-lottery'), // Page title
-        'Report', // Menu title
-        'manage_options',  // Capability
-        'report', // sub menu slug
-        'wxgiveaway_report_page' // sub meun funciton for page
-      );*/
-      //add submenu 5
-      
-      /*add_submenu_page(
-        'giveaway-system', // parent menu slug
-        __('Giveaway system license','giveaway-lottery'), // Page title
-        'License', // Menu title
-        'manage_options',  // Capability
-        'wxg-license', // sub menu slug
-        'wxgiveaway_license_page' // sub meun funciton for page
-      );*/
 
   }
 }
